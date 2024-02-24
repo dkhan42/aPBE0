546ca3ec3f17735d53ab3e4970f499a5533aa2d4
dkhan42 <66374468+dkhan42@users.noreply.github.com>
2024-02-24 12:40:39 -0500

    Update get_exchange.py

--- get_exchange.py
+++ get_exchange.py
@@ -10,20 +10,8 @@ def get_predictions(charges,coords):
     k = get_local_kernel(xtrain,rep,qtrain,charges,1638.4).T
     return (np.dot(k.T,alpha)/Ne)/100
 
-import numpy as np
-from qml.kernels import get_local_kernel
-from cMBDF import generate_mbdf
-from pyscf import gto, dft
-
-def get_predictions(charges,coords):
-    data = np.load('trained_model.npz', allow_pickle=True)
-    xtrain, qtrain, alpha = data['xtrain'], data['qtrain'], data['alpha']
-    rep = generate_mbdf(charges,coords,n_atm=2.0,pad=50)
-    Ne = np.array([np.sum(arr) for arr in charges])
-    k = get_local_kernel(xtrain,rep,qtrain,charges,1638.4).T
-    return (np.dot(k.T,alpha)/Ne)/100
-
 def get_atomization(elements, coords, exchange, basis='cc-pvtz'):
+    from pyscf import gto, dft
     a = exchange
 
     HF_X = a
